1882736adc6d4b14ec92f102b434ed9c6aefcde9
zhanhaixuan <465789851@qq.com>
2017-05-11 08:48:40 +0800

    添加meta表及功能

---
+++ lhzs-service/src/main/java/cn/lhzs/service/impl/MetaServiceImpl.java
@@ -0,0 +1,25 @@
+package cn.lhzs.service.impl;
+
+import cn.lhzs.data.bean.Meta;
+import cn.lhzs.data.dao.MetaMapper;
+import cn.lhzs.service.intf.MetaService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by ZHX on 2017/5/10.
+ */
+@Service
+@Transactional(readOnly = true)
+public class MetaServiceImpl implements MetaService{
+
+    @Resource
+    public MetaMapper metaMapper;
+
+    @Override
+    public Meta getMeta(int id) {
+        return metaMapper.selectByPrimaryKey(id);
+    }
+}

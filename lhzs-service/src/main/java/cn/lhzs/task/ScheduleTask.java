dd84622151dbf3ae2ba981dfe7ffef0cb1be03cf
zhanhaixuan <465789851@qq.com>
2017-08-09 01:05:53 +0800

    添加任务定时器，批量删除功能，修改店铺查询并编辑、删除功能问题

--- lhzs-service/src/main/java/cn/lhzs/task/ScheduleTask.java
+++ lhzs-service/src/main/java/cn/lhzs/task/ScheduleTask.java
@@ -3,13 +3,10 @@ package cn.lhzs.task;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-/**
- * Created by ZHX on 2017/5/15.
- */
-@Component("scheduleTask")
+@Component
 public class ScheduleTask {
-    @Scheduled(cron = "0 * 16 * * ?")
-    public void cleanTimeoutProduct(){
-        System.out.println("aaaaaaaaaaaaaa");
+    @Scheduled(cron = "0 0 2 * * ? ") // 间隔5秒执行
+    public void taskCycle() {
+        System.out.println("使用SpringMVC框架配置定时任务");
     }
 }

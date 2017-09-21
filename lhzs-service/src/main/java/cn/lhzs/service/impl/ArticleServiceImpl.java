2eab1e1c07a3c43f6deb2a1335dc9cffcb785eba
ZHX <465789851@qq.com>
2017-09-21 18:37:07 +0800

    修改代码

--- lhzs-service/src/main/java/cn/lhzs/service/impl/ArticleServiceImpl.java
+++ lhzs-service/src/main/java/cn/lhzs/service/impl/ArticleServiceImpl.java
@@ -78,73 +78,6 @@ public class ArticleServiceImpl implements ArticleService {
         return result;
     }
 
-//    @Override
-//    public JSONObject getArticleList(String data) {
-//        JSONObject jsonObject = JSONObject.parseObject(data).getJSONObject("reqData");
-//        Integer page = jsonObject.getInteger("page");
-//        Integer size = jsonObject.getInteger("pageSize");
-//        String type = jsonObject.getString("type");
-//        Article article = new Article();
-//        article.setPage(page);
-//        article.setSize(size);
-//        if (type != null) {
-//            if (type.length() == 1) {
-//                article.setSearchType("%" + type + "%");
-//                article.setNotSearchType("%" + type + type + "%");
-//            } else {
-//                article.setSearchType("%" + type + "%");
-//            }
-//        }
-//
-//        List<Article> articleList = getArticleList(article);
-//        List newList = new ArrayList();
-//        for (int i = 0; i < articleList.size(); i++) {
-//            Article article1 = articleList.get(i);
-//            String typeText = getTypeText(article1.getType());
-//            String time = new SimpleDateFormat("yyyy-MM-dd").format(article1.getCreateTime());
-//            if (typeText.length() > 10) {
-//                typeText = typeText.substring(0, 10) + "...";
-//            }
-//
-//            article1.setType(typeText);
-//            article1.setTime(time);
-//            newList.add(article1);
-//        }
-//
-//        Map countMap = new HashMap();
-//        if (type != null && !"".equals(type)) {
-//            countMap.put("searchType", type);
-//        }
-////        Integer count = articleMapper.selectCount(countMap);
-//        Integer count = 20;
-//        Integer totalPage = count / size;
-//        if (count % size != 0) {
-//            totalPage = totalPage + 1;
-//        }
-//
-//        JSONObject prodJson = new JSONObject();
-//        prodJson.put("articleList", newList);
-//        prodJson.put("totalPage", totalPage);
-//        prodJson.put("page", page);
-//
-//        return prodJson;
-//    }
-
-//    public List<Article> getArticleList(Article article) {
-//        Integer page = article.getPage();
-//        Integer size = article.getSize();
-//        if (page == null || size == null) {
-//            article.setIndex(0);
-//            article.setSize(20);
-//        } else {
-//            article.setIndex((page - 1) * size);
-//            article.setSize(size);
-//        }
-////        return articleMapper.selectListByType(article);
-//        return null;
-//    }
-
-
     private void generatorHtml(Article article) {
         try {
             //创建一个合适的Configration对象
@@ -186,8 +119,7 @@ public class ArticleServiceImpl implements ArticleService {
 
     @Override
     public int getArticleCount(Article article) {
-//        return articleMapper.selectCountByCondition(getSearchArticleCondition(article));
-        return 0;
+        return articleMapper.selectCountByExample(getSearchArticleCondition(article));
     }
 
     @Override
@@ -197,10 +129,8 @@ public class ArticleServiceImpl implements ArticleService {
 
     @Override
     public List<Article> searchArticle(Article article) {
-//        startPage(article.getPage(), article.getSize());
-//        return articleMapper.selectByCondition(getSearchArticleCondition(article));
-//        return articleMapper.selectAll();
-        return null;
+        startPage(article.getPage(), article.getSize());
+        return articleMapper.selectByExample(getSearchArticleCondition(article));
     }
 
     private Condition getSearchArticleCondition(Article article) {
